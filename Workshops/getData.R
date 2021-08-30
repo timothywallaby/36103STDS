ec22fdb87db54beaf868a8ed197b5cc7ba798730
timothywallaby <stevetimothy@gmail.com>
2021-08-30 21:45:40 +1000

    API Work

--- Workshops/getData.R
+++ Workshops/getData.R
@@ -51,3 +51,50 @@ eag.neac_dsd <- readSDMX(#we are going to use the same parameters as we filtered
 # httr
 library(httr)
 library(jsonlite)
+
+res = GET("https://zapier.com/learn/apis/chapter-2-protocols/")
+  #output returns a list
+
+res
+  #use the content-type to see what you can get out of this API
+  #from here you can extract data in the html file
+
+res2 = GET("http://api.open-notify.org/iss-pass.json",
+    query = list(lat = 40.7, lon = -74))
+  #check documentation of the API you are working with to see if you need any required query parameters
+
+data = fromJSON(rawToChar(res2$content))
+
+data$response
+
+#GET retrieve
+#POST make a resource with parameters
+#PUT change your order
+#DELETE delete a resource
+
+# token FPuXWiBep3nfBEIVGyK9twNidbuv
+# https://developers.amadeus.com/self-service/category/air/api-doc/flight-delay-prediction/api-reference
+
+# base url test.api.amadeus.com/v1
+# /airport/predictions/on-time
+
+token = "FPuXWiBep3nfBEIVGyK9twNidbuv"
+urlpath = "https://test.api.amadeus.com/v1/airport/predictions/on-time"
+delays <-  GET(
+          url = urlpath,
+          query = list(airportCode = "JFK", date="2020-12-01"),
+          add_headers(`Authorization` = sprintf("Token %s", token))
+          )
+delays
+
+delaydata = fromJSON(rawToChar(delays$content))
+
+delaydata$response
+
+Airlines <- GET( url = 'https://api.aviationstack.com/v1/airlines',
+                 'access_key' = 'fcd5517a6124bc5f203f3cbd7554a471')
+
+Airlines
+
+icao <- GET(
+  https://applications.icao.int/dataservices/api/safety-characteristics-list?api_key=8d00ef90-0982-11ec-9d72-8160549d64ab&airports=&states=USA&format=csv
